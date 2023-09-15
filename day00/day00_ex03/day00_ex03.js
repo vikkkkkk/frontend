30f4d84d323a15b9cdf6dd3b1ef34e34152dbd28
hsiuayes <hsiuayes@student.21-school.ru>
2023-09-15 13:12:40 +0300

    ex03

---
+++ day00/day00_ex03/day00_ex03.js
@@ -0,0 +1,28 @@
+/*
+
+Напишите функцию counter, которая при каждом вызове будет возвращать число на 3 больше, чем в прошлый.
+Нельзя использовать переменные, объявленные через var!
+
+*/
+
+function counter() {
+    let curCount = -3;
+    function recurCount() {
+        curCount += 3;
+        return curCount;
+    };
+    counter = recurCount;
+    return recurCount();
+}
+
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+
+/* 
+
+Тут используется замыкание (поскольку recurCount имеет доступ к переменной curCount внешней функции) и рекурсия для создания функции counter, 
+которая возвращает последовательные числа, увеличиваясь на 3
+
+*/
