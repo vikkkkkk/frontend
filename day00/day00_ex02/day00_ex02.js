592179a89c3e850ee00fcdf26b8833e3502dd7e1
hsiuayes <hsiuayes@student.21-school.ru>
2023-09-15 12:17:07 +0300

    ex02

--- day00/day00_ex02/day00_ex02.js
+++ day00/day00_ex02/day00_ex02.js
@@ -4,3 +4,22 @@
 Обязательно используйте методы и циклы, которые вы изучили.
 
 */
+
+function removeString(message, symbol) {
+    const arr = message.replace(new RegExp(symbol, 'g'), '');
+    return arr;
+}
+
+/* 
+
+"new RegExp(symbol, 'g')" создает регулярное выражение, которое будет искать все вхождения символа (или строки, представленной 
+в переменной symbol) в тексте 
+
+Когда используется это регулярное выражение с методом replace() и заменяется на пустую строку '', 
+это приводит к удалению всех вхождений этого символа из текста.
+
+*/
+
+console.log(removeString("Большое и интересное сообщение", "о"));
+console.log(removeString("Hello world!", "z"));
+console.log(removeString("А роза азора", "А"));
